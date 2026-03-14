520dc216b481828fa52f46ccc86fa66c6b8d8436
Shiny Chakma <127125066+chakma21@users.noreply.github.com>
2026-03-14 17:24:18 +0530

    Time: 1 ms (99.96%) | Memory: 42.8 MB (39.90%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int arrangeCoins(int n) {
+
+        int low = 0;
+        int high = n;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            long coins = (long) mid * (mid + 1) / 2;
+
+            if (coins <= n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return high;
+    }
+}
