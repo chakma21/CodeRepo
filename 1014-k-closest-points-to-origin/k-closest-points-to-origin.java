75b3b2010470576f744f8cf537092a77f44a756a
Shiny Chakma <127125066+chakma21@users.noreply.github.com>
2026-08-15 20:28:06 +0530

    Time: 22 ms (89.80%) | Memory: 55.7 MB (37.92%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
 
-        // Max heap based on distance
         PriorityQueue<int[]> pq =
-            new PriorityQueue<>((a, b) -> b[1] - a[1]);
+            new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
         for (int i = 0; i < points.length; i++) {
 
@@ -13,11 +12,6 @@ class Solution {
             int distance = x * x + y * y;
 
             pq.offer(new int[]{i, distance});
-
-            // Keep only k closest points
-            if (pq.size() > k) {
-                pq.poll();
-            }
         }
 
         int[][] ans = new int[k][2];
